fe2c443b3c79483a40a5da6e5da6a7c610afd7a1
Soumyar5176 <117309634+Soumyar5176@users.noreply.github.com>
2025-05-02 20:23:14 +0530

    Add files via upload

---
+++ assignment 1.py
@@ -0,0 +1,17 @@
+#  TASK 1
+
+a=input('enter the first number: ')
+b=input('enter the second number: ')
+a=int(a)
+b=int(b)
+print('addition: ',a+b)
+print('subtraction: ',a-b)
+print('multiplication: ',a*b)
+print('division: ',a/b)
+
+#  TASK 2
+
+a=input('enter your first name: ')
+b=input('enter your last name: ')
+print('Hello ',(a),(' '),(b), 'welcome to the python program.')
+
